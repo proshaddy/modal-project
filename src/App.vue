04435164fdb6a7284c5566d1c385f6394e00fc9e
shadrack kirop <shadrackkirop1197@gmail.com>
2023-02-11 14:26:06 +0300

    Changes

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  
+  <h1>{{ title }}</h1>
+  <div>
+    <ModalApp :header="header" :text="text" theme="sale" />
+  </div>
+</template>
+
+<script>
+import ModalApp from './components/ModalApp.vue'
+export default {
+  name: 'App',
+  components:{ ModalApp},
+  data(){
+    return {
+      title:'My first vue App:)',
+      header:'Sign up for the giveaway!',
+      text:'Grag ur ninja swag for half price',
+    }
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+ h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+
+}
+
+</style>
